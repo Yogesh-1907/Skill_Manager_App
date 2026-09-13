2e55210b6f849eb9e51821f0f0fd21090de351d2
Yogesh Choudhar <ychoudhar55@gmail.com>
2026-09-13 22:52:17 +0530

    Made some changes in files

--- controller/skill_controller.go
+++ controller/skill_controller.go
@@ -16,6 +16,8 @@ var (
 
 // Context is the most important part of gin. It allows us to pass variables between middleware, manage the flow,
 // validate the JSON of a request and render a JSON response for example.
+//comment
+
 type ISkillController interface {
 	FindBySkill(context *gin.Context)
 	CreateSkill(context *gin.Context)
